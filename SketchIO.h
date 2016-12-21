bb55faff7574124f11bf088e9393032b00025a03
Ozan Can Altiok <ozymaxx@gmail.com>
2016-12-21 16:53:53 +0200

    implementing sketch abstraction - not finished yet

--- SketchIO.h
+++ SketchIO.h
@@ -14,7 +14,19 @@ class SketchIO {
 		void setFileName( string filename);
 };
 
-// a new representation? (remember that we will also transfer them to CUDA device memory)
+class SketchNew {
+	private:
+		int numPoints,numStrokes;
+		int ptAdded,strAdded;
+		int **coords;
+		int *strokeIndices;
+	public:
+		SketchNew(int,int);
+		~SketchNew();
+		void addPoint(int,int);
+		void openStroke();
+		void printContents();
+};
 
 SketchIO::SketchIO( string filename) : filename(filename) {}
 
@@ -62,3 +74,48 @@ void SketchIO::read() {
 	
 	input.close();
 }
+
+SketchNew::SketchNew(int numPoints,int numStrokes) : numPoints(numPoints), numStrokes(numStrokes), ptAdded(0), strAdded(0) {
+	coords = new int*[numPoints];
+	for ( int i = 0; i < numPoints; ++i) {
+		coords[i] = new int[2];
+	}
+	
+	strokeIndices = new int[numStrokes];
+}
+
+SketchNew::~SketchNew() {
+	for ( int i = 0; i < numPoints; ++i) {
+		delete [] coords[i];
+	}
+	
+	delete [] coords;
+	
+	delete [] strokeIndices;
+}
+
+void SketchNew::addPoint(int x, int y) {
+	if (ptAdded < numPoints) {
+		coords[ptAdded][0] = x;
+		coords[ptAdded++][1] = y;
+	}
+	else {
+		cout << "ERROR: Sketch is full!" << endl;
+	}
+}
+
+void SketchNew::openStroke() {
+	if (strAdded < numStrokes) {
+		strokeIndices[strAdded++] = ptAdded;
+	}
+	else {
+		cout << "ERROR: Sketch is full!" << endl;
+	}
+}
+
+void SketchNew::printContents() {
+	for (int i = 0; i < numStrokes; ++i) {
+		for () { // left
+		}
+	}
+} 
