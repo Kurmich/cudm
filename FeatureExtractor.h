af27211b809f2d6169ff5eb5d3ac226e82ec7b7d
Kurmich <k.kaiyrbekov@gmail.com>
2016-12-22 12:31:05 +0300

    index problems fixed

--- FeatureExtractor.h
+++ FeatureExtractor.h
@@ -1,5 +1,6 @@
 #include "SketchIO.h"
 #include <math.h>
+#include <cmath>
 
 #define PI 3.14159265
 
@@ -13,18 +14,22 @@ class FeatureExtractor {
 
 FeatureExtractor::FeatureExtractor(Sketch* sketch) : sketch(sketch) {}
 
-void FeatureExtractor::coords2angles(int *&strokeIndices, double *&angles, int &numAngles) {
+void FeatureExtractor::coords2angles(int *&angleIndices, double *&angles, int &numAngles) {
+	//Get stroke coordinates and indices
 	int *sIndices = sketch->getStrokeIndices();
 	double **sCoords = sketch->getCoords();
 	
-	strokeIndices = new int[sketch->getNumStrokes()];
+	//Initialize 
+	angleIndices = new int[sketch->getNumStrokes()];
 	numAngles = sketch->getNumPoints() - sketch->getNumStrokes();
 	angles = new double[numAngles];
 	
 	int lastIndex;
 	double angle,diffy,diffx;
+	int curAngleIndex;
 	
 	for (int str = 0; str < sketch->getNumStrokes(); ++str) {
+		//Get last index of current stroke
 		if (str == sketch->getNumStrokes() - 1) {
 			lastIndex = sketch->getNumPoints();
 		}
@@ -32,19 +37,22 @@ void FeatureExtractor::coords2angles(int *&strokeIndices, double *&angles, int &
 			lastIndex = sIndices[str+1];
 		}
 		
-		strokeIndices[str] = sIndices[str]-1-str;
+		//Assign start index of angles for this stroke
+		angleIndices[str] = sIndices[str]-str;
+		//Starting index of angles to fill
+		curAngleIndex = angleIndices[str];
 		for (int pt = sIndices[str]+1; pt < lastIndex; ++pt) {
+			//Get differences both in x and y directions
 			diffy = sCoords[pt][1] - sCoords[pt-1][1];
 			diffx = sCoords[pt][0] - sCoords[pt-1][0];
 			
+			//Compute angle
 			angle = atan2(diffy,diffx);
-			
-			if ( angle < 0) {
-				angle += 2*PI;
-			}
-			
-			angle *= 180/PI;
-			angles[pt-str-1] = angle;
+			angle = fmod( (angle + 2*PI), (2*PI));
+			angle *= 180.0/PI;
+
+			//Assign current angle
+			angles[curAngleIndex++] = angle;
 		}
 	}
 }
