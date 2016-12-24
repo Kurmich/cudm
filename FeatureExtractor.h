53c2666997c0353e4c3844467b3d0a4df8b14993
Kurmich <k.kaiyrbekov@gmail.com>
2016-12-24 21:14:11 +0300

    smoothing and downsampling left

--- FeatureExtractor.h
+++ FeatureExtractor.h
@@ -18,12 +18,188 @@ class FeatureExtractor {
 		void setSketch(Sketch* newSketch);
 		vector<int> arange(int a, int b, int step);
 		vector<int> cum(vector<int> q,  int x, char op);
-		void drawBresenham(  double x1,  double y1, double x2, double y2 );
+		void drawBresenham(  double x1,  double y1, double x2, double y2, double* pixels, int angleIndex, double** &image );
 		double** init2Darray(int size);
-		double** extractFeatureImage(double* pixels, Sketch* transformed, double sigma, double hsize, int gridWidth, int gridHeight );
+		double** extractFeatureImage(double* pixels, int* angleIndices, int numAngles, Sketch* transformed, double sigma, double hsize, int gridSize, bool endpt);
+		void pointsToImage(double* pixels, int* angleIndices, double** sCoords, int gridSize, int strokeStart, int strokeEnd, int angleStart, int angleEnd, double** &image);
 };
 
 FeatureExtractor::FeatureExtractor(Sketch* sketch) : sketch(sketch) {}
+
+
+
+void FeatureExtractor::setSketch(Sketch* newSketch)
+{
+	sketch = newSketch;
+}
+
+double* FeatureExtractor::extract()
+{
+	
+	double resampleInterval = 50.0;
+    double sigma = 10.0;
+    double hsize = 4.0;
+    int gridSize = 12;
+   // const double angles[] = {0, 45, 90, 135, 180 };
+    int idmFeatureSize = gridSize*gridSize*5;
+    double* idmFeature = new double[idmFeatureSize];
+    Sketch* resampled = sketch->resample(resampleInterval);
+    cout<<"resampled"<<endl;
+    resampled->printContents();
+    Sketch* normalized = resampled->normalized();
+    cout<<"normalized"<<endl;
+    normalized->printContents();
+    int *angleIndices;
+	double *angles;
+	int numAngles;
+	double minX, minY, maxX, maxY;
+	int numOfStrokes = normalized->getNumStrokes();
+	setSketch(normalized);
+	coords2angles(angleIndices,angles,numAngles, maxX, maxY, minX, minY);
+	Sketch* transformed = normalized->transform(minX, minY, maxX, maxY);
+	cout<<"transformed"<<endl;
+	transformed->printContents();
+	int curAngle = 0;
+	double* pixels1 = pixelValues(angles, curAngle, (curAngle + 180)%360, numAngles);
+	double** featImage1 = extractFeatureImage(pixels1, angleIndices, numAngles, transformed, sigma, hsize,  gridSize, false );
+	curAngle = 45;
+	double* pixels2 = pixelValues(angles, curAngle, (curAngle + 180)%360, numAngles);
+	double** featImage2 = extractFeatureImage(pixels1, angleIndices, numAngles, transformed, sigma, hsize,  gridSize, false );
+	curAngle = 90;
+	double* pixels3 = pixelValues(angles, curAngle, (curAngle + 180)%360, numAngles);
+	double** featImage3 = extractFeatureImage(pixels1, angleIndices, numAngles, transformed, sigma, hsize,  gridSize, false );
+	curAngle = 135;
+	double* pixels4 = pixelValues(angles, curAngle, (curAngle + 180)%360, numAngles);
+	double** featImage4 = extractFeatureImage(pixels1, angleIndices, numAngles, transformed, sigma, hsize,  gridSize, false );
+	double* pixels5 = pixelValues(angles, curAngle, (curAngle + 180)%360, numAngles);
+	double** featImage5 = extractFeatureImage(pixels1, angleIndices, numAngles, transformed, sigma, hsize,  gridSize, true );
+	/*
+	for(int i = 0; i < 2*gridSize; ++i)
+	{
+		for(int j = 0; j < 2*gridSize; ++j)
+		{
+			cout<<featImage1[i][j]<< " ";
+		}
+		cout<<endl;
+	}
+
+	cout << "pixels = " << endl;
+	for (int i = 0; i < numAngles; ++i) {
+		cout << pixels[i] << endl;
+	}*/
+	
+
+    return idmFeature;
+}
+
+double** FeatureExtractor::extractFeatureImage(double* pixels, int* angleIndices, int numAngles, Sketch* transformed, double sigma, double hsize, int gridSize, bool endpt )
+{
+	double** featim = init2Darray(2*gridSize);;
+	int* sIndices = transformed->getStrokeIndices();
+	double** sCoords = transformed->getCoords();
+	int numOfPoints = transformed->getNumPoints();  
+	int numOfStrokes = transformed->getNumStrokes();
+	if(!endpt)
+	{
+		int strokeStart, strokeEnd, angleStart, angleEnd;
+		for(int i = 0; i < numOfStrokes; ++i)
+		{
+			strokeStart = sIndices[i];
+			angleStart = angleIndices[i];
+			if(i < numOfStrokes - 1 )
+			{
+				strokeEnd = sIndices[i+1];
+				angleEnd = angleIndices[i+1]; 
+			}
+			else
+			{
+				angleEnd = numAngles;
+				strokeEnd = numOfPoints;
+			}
+			cout<<"pointsToImage "<<i<<endl;
+			pointsToImage(pixels, angleIndices, sCoords, gridSize, strokeStart, strokeEnd, angleStart, angleEnd, featim);
+		}
+		
+	}
+	else
+	{
+		int strokeStart, strokeEnd;
+		for(int i = 0; i < numOfStrokes; ++i)
+		{
+			strokeStart = sIndices[i];
+			if(i < numOfStrokes - 1 )
+			{
+				strokeEnd = sIndices[i+1] - 1; 
+			}
+			else
+			{
+				strokeEnd = numOfPoints - 1;
+			}
+			featim[ (int)sCoords[strokeStart][1] ][ (int)sCoords[strokeStart][0] ] = 1;
+			featim[ (int)sCoords[strokeEnd][1] ][ (int)sCoords[strokeEnd][0] ] = 1;
+		}
+	}
+
+
+	for(int i = 0; i < 2*gridSize; ++i)
+	{
+		for(int j = 0; j < 2*gridSize; ++j)
+		{
+			cout<<featim[i][j]<< " ";
+		}
+		cout<<endl;
+	}
+
+	cout << "pixels = " << endl;
+	for (int i = 0; i < numAngles; ++i) {
+		cout << pixels[i] << endl;
+	}
+	return featim; 
+}
+
+void FeatureExtractor::pointsToImage(double* pixels, int* angleIndices, double** sCoords, int gridSize, int strokeStart, int strokeEnd, int angleStart, int angleEnd, double** &image)
+{
+	int numOfAngles = angleEnd - angleStart;
+	cout<<"number of angles"<<numOfAngles<<endl;
+	if(numOfAngles == 0)
+	{
+		return;
+	}
+	int angleIndex, pointIndex;
+	double x1, x2, y1, y2;
+	for(int i = 0; i < numOfAngles; ++i)
+	{
+		angleIndex = angleStart + i;
+		pointIndex = strokeStart + i;
+		x1 = sCoords[pointIndex][0];
+		x2 = sCoords[pointIndex+1][0];
+		y1 = sCoords[pointIndex][1];
+		y2 = sCoords[pointIndex+1][1];
+		if(pixels[angleIndex] > 0)
+		{
+			drawBresenham(  x1,  y1, x2, y2, pixels, angleIndex, image );
+		}
+	}
+	
+}
+
+double** FeatureExtractor::init2Darray(int size)
+{
+	double** array = new double*[size];
+	for(int i =0; i < size; ++i)
+	{
+		array[i] = new double[size];
+	}
+
+	for(int i = 0; i < size; ++i)
+	{
+		for(int j = 0; j < size; ++j)
+		{
+			array[i][j] = 0;
+		}
+	}
+	return array;
+}
 
 vector<int> FeatureExtractor::arange(int a, int b, int step)
 {
@@ -65,7 +241,7 @@ vector<int> FeatureExtractor::cum(vector<int> q,  int x, char op)
   return ans;
 }
 
-void FeatureExtractor::drawBresenham(  double x1,  double y1, double x2, double y2 )
+void FeatureExtractor::drawBresenham(  double x1,  double y1, double x2, double y2, double* pixels, int angleIndex, double** &image )
 {
   x1 = round(x1);
   x2 = round(x2);
@@ -156,91 +332,18 @@ void FeatureExtractor::drawBresenham(  double x1,  double y1, double x2, double 
       y = cum(q, y1, '-');
     }
   }
-
+   cout<<"bresenham angle index "<<angleIndex<<endl;
   for(int i = 0; i < x.size(); ++i)
   {
-        cout<<x[i]<<" "<<y[i]<<endl;
+        cout<<"bresenham "<<x[i]<<" "<<y[i]<<endl;
+        if(image[ y[i] ][ x[i] ] < pixels[angleIndex])
+        {
+        	image[ y[i] ][ x[i] ] = pixels[angleIndex];
+        }
   }
-  cout<<dx<< " " <<dy<<endl;
+  //cout<<dx<< " " <<dy<<endl;
 }
 
-
-void FeatureExtractor::setSketch(Sketch* newSketch)
-{
-	sketch = newSketch;
-}
-
-double* FeatureExtractor::extract()
-{
-	
-	double resampleInterval = 50.0;
-    double sigma = 10.0;
-    double hsize = 4.0;
-    int gridWidth = 12; 
-    int gridHeight = 12;
-   // const double angles[] = {0, 45, 90, 135, 180 };
-    int idmFeatureSize = gridWidth*gridHeight*5;
-    double* idmFeature = new double[idmFeatureSize];
-    Sketch* resampled = sketch->resample(resampleInterval);
-    cout<<"resampled"<<endl;
-    resampled->printContents();
-    Sketch* normalized = resampled->normalized();
-    cout<<"normalized"<<endl;
-    normalized->printContents();
-    int *angleIndices;
-	double *angles;
-	int numAngles;
-	double minX, minY, maxX, maxY;
-	int numOfStrokes = normalized->getNumStrokes();
-	setSketch(normalized);
-	coords2angles(angleIndices,angles,numAngles, maxX, maxY, minX, minY);
-	Sketch* transformed = normalized->transform(minX, minY, maxX, maxY);
-	cout<<"transformed"<<endl;
-	transformed->printContents();
-	double* pixels = pixelValues(angles, 45, 45, numAngles);
-	double** featImage1 = extractFeatureImage(pixels, transformed, sigma, hsize, gridWidth, gridHeight );
-	/*
-	for(int i = 0; i < 2*gridWidth; ++i)
-	{
-		for(int j = 0; j < 2*gridWidth; ++j)
-		{
-			cout<<featImage1[i][j];
-		}
-		cout<<endl;
-	}
-
-	cout << "pixels = " << endl;
-	for (int i = 0; i < numAngles; ++i) {
-		cout << pixels[i] << endl;
-	}
-	*/
-
-    return idmFeature;
-}
-
-double** FeatureExtractor::extractFeatureImage(double* pixels, Sketch* transformed, double sigma, double hsize, int gridWidth, int gridHeight )
-{
-	double** featim = init2Darray(2*gridWidth);
-	return featim; 
-}
-
-double** FeatureExtractor::init2Darray(int size)
-{
-	double** array = new double*[size];
-	for(int i =0; i < size; ++i)
-	{
-		array[i] = new double[size];
-	}
-
-	for(int i = 0; i < size; ++i)
-	{
-		for(int j = 0; j < size; ++j)
-		{
-			array[i][j] = 0;
-		}
-	}
-	return array;
-}
 
 void FeatureExtractor::coords2angles(int *&angleIndices, double *&angles, int &numOfAngles, double &maxX, double &maxY, double &minX, double &minY) {
 	//Get stroke coordinates and indices
