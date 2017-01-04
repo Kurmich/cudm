14576745add3c969cf8f062e8db34d0a78c86d5a
Ozan Can Altiok <ozymaxx@gmail.com>
2017-01-04 13:55:11 +0200

    OpenMP parallelization has been added

--- FeatureExtractor.h
+++ FeatureExtractor.h
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <cmath>
 #include <vector>
+#include <limits>
 
 #define PI 3.14159265
 #define BLOCK_SIZE 512
@@ -36,27 +37,56 @@ void FeatureExtractor::setSketch(Sketch* newSketch)
 void FeatureExtractor::findExtremum(double &maxX, double &maxY, double &minX, double &minY) {
 	double** sCoords = sketch->getCoords();
 	int numPoints = sketch->getNumPoints();
+	double my_minX,my_minY,my_maxX,my_maxY;
 	
-	minX = sCoords[0][0];
-	minY = sCoords[1][0];
-	maxX = sCoords[0][0];
-	maxY = sCoords[1][0];
+	minX = numeric_limits<double>::max();
+	minY = numeric_limits<double>::max();
+	maxX = numeric_limits<double>::min();
+	maxY = numeric_limits<double>::min();
 	
-	for (int i = 1; i < numPoints; ++i) {
-		if (minX > sCoords[0][i]) {
-			minX = sCoords[0][i];
+	#pragma omp parallel private(my_minX,my_minY,my_maxX,my_maxY) num_threads(32)
+	{
+		my_minX = numeric_limits<double>::max();
+		my_minY = numeric_limits<double>::max();
+		my_maxX = numeric_limits<double>::min();
+		my_maxY = numeric_limits<double>::min();
+		
+		#pragma omp for
+		for (int i = 0; i < numPoints; ++i) {
+			if (my_minX > sCoords[0][i]) {
+				my_minX = sCoords[0][i];
+			}
+			
+			if (my_minY > sCoords[1][i]) {
+				my_minY = sCoords[1][i];
+			}
+			
+			if (my_maxX < sCoords[0][i]) {
+				my_maxX = sCoords[0][i];
+			}
+			
+			if (my_maxY < sCoords[1][i]) {
+				my_maxY = sCoords[1][i];
+			}
 		}
 		
-		if (minY > sCoords[1][i]) {
-			minY = sCoords[1][i];
-		}
-		
-		if (maxX < sCoords[0][i]) {
-			maxX = sCoords[0][i];
-		}
-		
-		if (maxY < sCoords[1][i]) {
-			maxY = sCoords[1][i];
+		#pragma omp critical
+		{
+			if (my_minX < minX) {
+				minX = my_minX;
+			}
+			
+			if (my_minY < minY) {
+				minY = my_minY;
+			}
+			
+			if (my_maxX > maxX) {
+				maxX = my_maxX;
+			}
+			
+			if (my_maxY > maxY) {
+				maxY = my_maxY;
+			}
 		}
 	}
 }
@@ -99,16 +129,33 @@ double* FeatureExtractor::extract()
 	//transformed->printContents();
 	double** gfilter = gaussianFilter(hsize, sigma);
 	
-	double** featImage1 = extractFeatureImage(gfilter,pixels, angleIndices, numAngles, transformed,  hsize,  gridSize, false );
-	double** featImage2 = extractFeatureImage(gfilter,&pixels[numAngles], angleIndices, numAngles, transformed,  hsize,  gridSize, false );
-	double** featImage3 = extractFeatureImage(gfilter,&pixels[2*numAngles], angleIndices, numAngles, transformed,  hsize,  gridSize, false );
-	double** featImage4 = extractFeatureImage(gfilter,&pixels[3*numAngles], angleIndices, numAngles, transformed,  hsize,  gridSize, false );
-	double** featImage5 = extractFeatureImage(gfilter,pixels, angleIndices, numAngles, transformed,  hsize,  gridSize, true );
+	double **featImage1, **featImage2, **featImage3, **featImage4, **featImage5;
+	#pragma omp parallel num_threads(8)
+	{
+		#pragma omp sections
+		{
+			#pragma omp section
+			featImage1 = extractFeatureImage(gfilter,pixels, angleIndices, numAngles, transformed,  hsize,  gridSize, false );
+			
+			#pragma omp section
+			featImage2 = extractFeatureImage(gfilter,&pixels[numAngles], angleIndices, numAngles, transformed,  hsize,  gridSize, false );
+			
+			#pragma omp section
+			featImage3 = extractFeatureImage(gfilter,&pixels[2*numAngles], angleIndices, numAngles, transformed,  hsize,  gridSize, false );
+			
+			#pragma omp section
+			featImage4 = extractFeatureImage(gfilter,&pixels[3*numAngles], angleIndices, numAngles, transformed,  hsize,  gridSize, false );
+			
+			#pragma omp section
+			featImage5 = extractFeatureImage(gfilter,pixels, angleIndices, numAngles, transformed,  hsize,  gridSize, true );
+		}
+	}
 	
 	delete [] pixels;
 	delete [] curAngles;
 	delete [] curAngles2;
 
+	#pragma omp parallel for num_threads(32)
 	for (int i = 0; i < gridSize; ++i) {
 		for (int j = 0; j < gridSize; ++j) {
 			idmFeature[i*gridSize+j] = featImage1[i][j];
@@ -203,6 +250,8 @@ double** FeatureExtractor::smoothim(double **image, double** fgauss, int hsize, 
 	double** result = init2Darray(2*gridSize);
 	int ax, ay;
 	double sum = 0, maximum = 0;
+	
+	#pragma omp parallel for num_threads(32)
 	for(int i = 0; i < 2*gridSize; ++i)
 	{
 		for(int j = 0; j < 2*gridSize; ++j)
@@ -221,11 +270,16 @@ double** FeatureExtractor::smoothim(double **image, double** fgauss, int hsize, 
 				}
 			}
 			result[i][j] = sum;
-			maximum = max(maximum, sum);
+			
+			if (sum > maximum) {
+				maximum = sum;
+			}
 		}
 	}
+	
 	if(maximum != 0)
 	{
+		#pragma omp parallel for num_threads(32)
 		for(int i = 0; i < 2*gridSize; ++i)
 		{
 			for(int j = 0; j < 2*gridSize; ++j)
@@ -243,13 +297,26 @@ double** FeatureExtractor::downsample(double** image, double gridSize)
 {
 	double** downsampled = init2Darray(gridSize);
 	double curMax;
+	
 	for(int i = 0; i < 2*gridSize; i += 2)
 	{
 		for(int j = 0; j < 2*gridSize; j += 2)
 		{
-			curMax = max(image[i][j], image[i+1][j]);
-			curMax = max(curMax, image[i][j+1]);
-			curMax = max(curMax, image[i+1][j+1]);
+			if (image[i][j] < image[i+1][j]) {
+				curMax = image[i+1][j];
+			}
+			else {
+				curMax = image[i][j];
+			}
+			
+			if (curMax < image[i][j+1]) {
+				curMax = image[i][j+1];
+			}
+			
+			if (curMax < image[i+1][j+1]) {
+				curMax = image[i+1][j+1];
+			}
+			
 			downsampled[i/2][j/2] = curMax;
 		}
 	}
@@ -265,6 +332,7 @@ void FeatureExtractor::pointsToImage(double* pixels, int* angleIndices, double**
 	{
 		return;
 	}
+	
 	int angleIndex, pointIndex;
 	double x1, x2, y1, y2;
 	for(int i = 0; i < numOfAngles; ++i)
@@ -287,6 +355,7 @@ void FeatureExtractor::pointsToImage(double* pixels, int* angleIndices, double**
 double** FeatureExtractor::init2Darray(int size)
 {
 	double** array = new double*[size];
+	
 	for(int i =0; i < size; ++i)
 	{
 		array[i] = new double[size];
@@ -299,151 +368,161 @@ double** FeatureExtractor::init2Darray(int size)
 			array[i][j] = 0;
 		}
 	}
+	
 	return array;
 }
 
 vector<int> FeatureExtractor::arange(int a, int b, int step)
 {
-  vector<int> ans;
-  if(step == 1)
-  {
-    for(int i = a; i <= b; ++i)
-    {
-      ans.push_back(i);
-    }
-  }
-  else
-  {
-    for(int i = a; i >= b; --i)
-    {
-      ans.push_back(i);
-    }
-  }
-  return ans;
+	vector<int> ans;
+	
+	if(step == 1)
+	{
+		for(int i = a; i <= b; ++i)
+		{
+			ans.push_back(i);
+		}
+	}
+	else
+	{
+		for(int i = a; i >= b; --i)
+		{
+			ans.push_back(i);
+		}
+	}
+	
+	return ans;
 }
 
 vector<int> FeatureExtractor::cum(vector<int> q,  int x, char op)
 {
-  vector<int> ans;
-  if(op == '+')
-  {
-    for(int i = 0; i < q.size(); ++i)
-      {
-        ans.push_back(q[i] + x );
-      }
-  }
-  else
-  {
-    for(int i = 0; i < q.size(); ++i)
-      {
-        ans.push_back( x - q[i] );
-      }
-  }
-  return ans;
+	vector<int> ans;
+	
+	if(op == '+')
+	{
+		for(int i = 0; i < q.size(); ++i)
+		{
+			ans.push_back(q[i] + x );
+		}
+	}
+	else
+	{
+		for(int i = 0; i < q.size(); ++i)
+		{
+			ans.push_back( x - q[i] );
+		}
+	}
+	
+	return ans;
 }
 
 void FeatureExtractor::drawBresenham(  double x1,  double y1, double x2, double y2, double* pixels, int angleIndex, double** &image )
 {
-  x1 = round(x1);
-  x2 = round(x2);
-  y1 = round(y1);
-  y2 = round(y2);
-  int dx = abs(x2 - x1);
-  int dy = abs(y2 - y1);
-  bool steep = abs(dy) > abs(dx);
-  vector<int> q;
+	x1 = round(x1);
+	x2 = round(x2);
+	y1 = round(y1);
+	y2 = round(y2);
+	int dx = abs(x2 - x1);
+	int dy = abs(y2 - y1);
+	bool steep = abs(dy) > abs(dx);
+	vector<int> q;
 
-  if(steep)
-  {
-    int tmp = dx;
-    dx = dy;
-    dy = tmp;
-  }
-  if(dy == 0)
-  {
-    for(int  i = 0; i <= dx; ++i)
-    {
-      q.push_back(0);
-    }
-  }
-  else
-  {
-    q.push_back(0);
-    vector<int> arr;
-    int cur;
-    int cumSum = 0;
-    for(int i = floor(dx/2); i >= floor(dx/2) - dy*dx; i -= dy)
-    {
-      cur = (i + dx)%dx;
-      while(cur < 0)
-      {
-        cur = (cur + dx)%dx;
-      }
-      arr.push_back(cur);
-     // cout<<cur<<endl;
-    }
-    for(int i = 1; i < arr.size(); ++i)
-      {
-        cur = arr[i] - arr[i-1];
-        cur = cur >=0?1:0;
-        cumSum += cur;
-        q.push_back(cumSum);
-       // cout<<cumSum<<endl;
-      }
-  }
-  vector<int> y;
-  vector<int> x;
-  if(steep)
-  {
-    if(y1 <= y2)
-    {
-      y = arange(y1, y2, 1);
-    }
-    else
-    {
-      y = arange(y1, y2, -1);
-    }
+	if(steep)
+	{
+		int tmp = dx;
+		dx = dy;
+		dy = tmp;
+	}
+	
+	if(dy == 0)
+	{
+		for(int  i = 0; i <= dx; ++i)
+		{
+			q.push_back(0);
+		}
+	}
+	else
+	{
+		q.push_back(0);
+		vector<int> arr;
+		int cur;
+		int cumSum = 0;
+		
+		for(int i = floor(dx/2); i >= floor(dx/2) - dy*dx; i -= dy)
+		{
+			cur = (i + dx)%dx;
+			
+			while(cur < 0)
+			{
+				cur = (cur + dx)%dx;
+			}
+			arr.push_back(cur);
+			// cout<<cur<<endl;
+		}
+		
+		for(int i = 1; i < arr.size(); ++i)
+		{
+			cur = arr[i] - arr[i-1];
+			cur = cur >=0?1:0;
+			cumSum += cur;
+			q.push_back(cumSum);
+			// cout<<cumSum<<endl;
+		}
+	}
+	vector<int> y;
+	vector<int> x;
+	if(steep)
+	{
+		if(y1 <= y2)
+		{
+			y = arange(y1, y2, 1);
+		}
+		else
+		{
+			y = arange(y1, y2, -1);
+		}
 
-    if(x1 <= x2)
-    {
-      x = cum(q, x1, '+');
-    }
-    else
-    {
-      x = cum(q, x1, '-');
-    }
-  }
-  else
-  {
-    if(x1 <= x2)
-    {
-      x = arange(x1, x2, 1);
-    }
-    else
-    {
-      x = arange(x1, x2, -1);
-    }
+		if(x1 <= x2)
+		{
+			x = cum(q, x1, '+');
+		}
+		else
+		{
+			x = cum(q, x1, '-');
+		}
+	}
+	else
+	{
+		if(x1 <= x2)
+		{
+			x = arange(x1, x2, 1);
+		}
+		else
+		{
+			x = arange(x1, x2, -1);
+		}
 
-    if(y1 <= y2)
-    {
-      y = cum(q, y1, '+');
-    }
-    else
-    {
-      y = cum(q, y1, '-');
-    }
-  }
-  //cout<<"Hello"<<endl;
- // cout<<"bresenham angle index "<<angleIndex<<endl;
-  for(int i = 0; i < x.size(); ++i)
-  {
+		if(y1 <= y2)
+		{
+			y = cum(q, y1, '+');
+		}
+		else
+		{
+			y = cum(q, y1, '-');
+		}
+	}
+	
+	//cout<<"Hello"<<endl;
+	// cout<<"bresenham angle index "<<angleIndex<<endl;
+	for(int i = 0; i < x.size(); ++i)
+	{
 		//cout<<"bresenham "<<x[i]<<" "<<y[i]<<endl;
-        if(image[ y[i] ][ x[i] ] < pixels[angleIndex])
-        {
-        	image[ y[i] ][ x[i] ] = pixels[angleIndex];
-        }
-  }
-  //cout<<dx<< " " <<dy<<endl;
+		if(image[ y[i] ][ x[i] ] < pixels[angleIndex])
+		{
+			image[ y[i] ][ x[i] ] = pixels[angleIndex];
+		}
+	}
+	//cout<<dx<< " " <<dy<<endl;
 }
 
 __global__ void coords2pixels_kernel(double *sCoords_x, double *sCoords_y, double *curAngle, double *curAngle2, double *angleThreshold, int *numAngles, int *strokes, int *strokeIndices, int *numOfPoints, double *pixValues) {
@@ -532,6 +611,7 @@ void FeatureExtractor::coords2pixels(int *&angleIndices, double *curAngles, doub
 			lastIndex = sIndices[str+1];
 		}
 		
+		#pragma omp parallel for num_threads(32)
 		for (int pt = sIndices[str]; pt < lastIndex; ++pt) {
 			strokes[pt] = str;
 		}
@@ -568,10 +648,24 @@ void FeatureExtractor::coords2pixels(int *&angleIndices, double *curAngles, doub
 	cudaMemcpy( numAngles_device, &numOfAngles, sizeof(int), cudaMemcpyHostToDevice);
 	
 	int itemsPerBlock = numOfPoints / BLOCK_SIZE + (numOfPoints % BLOCK_SIZE == 0 ? 0 : 1);
-	coords2pixels_kernel<<<itemsPerBlock,BLOCK_SIZE>>>(sCoords_x_device,sCoords_y_device,&curAngles_device[1],&curAngles2_device[1],angleThreshold_device,numAngles_device,strokes_device,strokeIndices_device,numOfPoints_device,pixValues_device);
-	coords2pixels_kernel<<<itemsPerBlock,BLOCK_SIZE>>>(sCoords_x_device,sCoords_y_device,&curAngles_device[0],&curAngles2_device[0],angleThreshold_device,numAngles_device,strokes_device,strokeIndices_device,numOfPoints_device,pixValues_device);	
-	coords2pixels_kernel<<<itemsPerBlock,BLOCK_SIZE>>>(sCoords_x_device,sCoords_y_device,&curAngles_device[2],&curAngles2_device[2],angleThreshold_device,numAngles_device,strokes_device,strokeIndices_device,numOfPoints_device,pixValues_device);
-	coords2pixels_kernel<<<itemsPerBlock,BLOCK_SIZE>>>(sCoords_x_device,sCoords_y_device,&curAngles_device[3],&curAngles2_device[3],angleThreshold_device,numAngles_device,strokes_device,strokeIndices_device,numOfPoints_device,pixValues_device);
+	
+	#pragma omp parallel num_threads(4)
+	{
+		#pragma omp sections
+		{
+			#pragma omp section
+			coords2pixels_kernel<<<itemsPerBlock,BLOCK_SIZE>>>(sCoords_x_device,sCoords_y_device,&curAngles_device[1],&curAngles2_device[1],angleThreshold_device,numAngles_device,strokes_device,strokeIndices_device,numOfPoints_device,pixValues_device);
+			
+			#pragma omp section
+			coords2pixels_kernel<<<itemsPerBlock,BLOCK_SIZE>>>(sCoords_x_device,sCoords_y_device,&curAngles_device[0],&curAngles2_device[0],angleThreshold_device,numAngles_device,strokes_device,strokeIndices_device,numOfPoints_device,pixValues_device);	
+			
+			#pragma omp section
+			coords2pixels_kernel<<<itemsPerBlock,BLOCK_SIZE>>>(sCoords_x_device,sCoords_y_device,&curAngles_device[2],&curAngles2_device[2],angleThreshold_device,numAngles_device,strokes_device,strokeIndices_device,numOfPoints_device,pixValues_device);
+			
+			#pragma omp section
+			coords2pixels_kernel<<<itemsPerBlock,BLOCK_SIZE>>>(sCoords_x_device,sCoords_y_device,&curAngles_device[3],&curAngles2_device[3],angleThreshold_device,numAngles_device,strokes_device,strokeIndices_device,numOfPoints_device,pixValues_device);
+		}
+	}
 	
 	cudaError_t err = cudaGetLastError();
 	if (err != cudaSuccess) cout << "CUDA Execution Error: " << cudaGetErrorString(err) << endl;
